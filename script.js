aeb1217fe11629b03ac2ed13450ce8fb4f75559e
Jamie Garrett <garrettjamie87@googlemail.com>
2021-02-17 16:09:07 +0100

    create function blurring()  which takes load counter and increments by 1

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 const loadingText = document.querySelector('.loading-text');
 const backgroundImage = document.querySelector('.bg');
+
+let load = 0
+
+function blurring() {
+      load++
+
+      console.log(load)
+}
